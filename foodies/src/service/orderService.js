7977749b388971cd6e5610174433a508ee693505
Ishika Agrawal <147633093+ishika2811@users.noreply.github.com>
2026-04-16 21:45:27 +0530

    Update API_URL to use production endpoint

--- foodies/src/service/orderService.js
+++ foodies/src/service/orderService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/orders";
+const API_URL = "https://foodies-28.onrender.com/api"
 
 export const fetchUserOrders = async (token) => {
     try {
