01333d228ac9bf0052a053ca1ebe0a785e9e4c0b
Ishika Agrawal <147633093+ishika2811@users.noreply.github.com>
2026-04-16 21:44:53 +0530

    Update API URL to production endpoint

--- foodies/src/service/cartService.js
+++ foodies/src/service/cartService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/cart";
+const API_URL = "https://foodies-28.onrender.com/api"
 
 export const addToCart = async (foodId, token) => {
     try {
