e95e30491462d6e4ec259dbaaa62f8e68fef9276
Ishika Agrawal <147633093+ishika2811@users.noreply.github.com>
2026-04-17 10:44:33 +0530

    Update AWSConfig.java

--- src/main/java/in/ishikag/foodiesapi/config/AWSConfig.java
+++ src/main/java/in/ishikag/foodiesapi/config/AWSConfig.java
@@ -1,27 +1,33 @@
 package in.ishikag.foodiesapi.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 
-// @Configuration
+@Configuration
 public class AWSConfig {
+
     @Value("${aws.access.key}")
-    private String accesskey;
+    private String accessKey;
+
     @Value("${aws.secret.key}")
-    private String secretkey;
+    private String secretKey;
+
     @Value("${aws.region}")
     private String region;
 
     @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accesskey, secretkey)))
+    public AmazonS3 amazonS3() {
+        BasicAWSCredentials credentials =
+                new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
     }
 }
