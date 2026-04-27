dd6e9e4f1846245a66ee709b702fab776127121e
Ishika Agrawal <147633093+ishika2811@users.noreply.github.com>
2026-04-27 21:44:53 +0530

    Enhance user details loading with role authority

--- src/main/java/in/ishikag/foodiesapi/service/AppUserDetailsService.java
+++ src/main/java/in/ishikag/foodiesapi/service/AppUserDetailsService.java
@@ -7,9 +7,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -17,11 +18,16 @@ public class AppUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
         UserEntity user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return new User(user.getEmail(), user.getPassword(), Collections.emptyList());
+
+        return new User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
     }
 }
