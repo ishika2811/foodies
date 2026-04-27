8ef395b592fff7fcdb3cecccf31cea5ad3147478
Ishika Agrawal <147633093+ishika2811@users.noreply.github.com>
2026-04-27 23:19:05 +0530

    Update SecurityConfig.java

--- src/main/java/in/ishikag/foodiesapi/config/SecurityConfig.java
+++ src/main/java/in/ishikag/foodiesapi/config/SecurityConfig.java
@@ -65,9 +65,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(
-               "*"
-        ));
+       config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"
         ));
